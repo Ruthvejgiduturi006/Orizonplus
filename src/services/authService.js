d4bb1c60e174c9d1e4f3a0bf8401d4c28c865684
Ruthvejgiduturi006 <Ruthvejgiduturi006@gmail.com>
2025-05-08 12:46:03 +0530

    Updated server and front-end files, added new Job-related files

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,33 +1,18 @@
 // services/authService.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 export const signup = async (userData) => {
-  try {
-    const res = await axios.post(`${API_URL}/signup`, userData);
-    
-    // Save token + user to localStorage
-    localStorage.setItem('token', res.data.token);
-    localStorage.setItem('user', JSON.stringify(res.data.user));
-    
-    return res.data;
-  } catch (err) {
-    console.error('Signup Failed:', err.response?.data || err.message);
-    throw err;
-  }
+  const res = await axios.post(`${API_BASE}/auth/signup`, userData);
+  localStorage.setItem('token', res.data.token);
+  localStorage.setItem('user', JSON.stringify(res.data.user));
+  return res.data;
 };
 
 export const login = async (userData) => {
-  try {
-    const res = await axios.post(`${API_URL}/login`, userData);
-    
-    localStorage.setItem('token', res.data.token);
-    localStorage.setItem('user', JSON.stringify(res.data.user));
-    
-    return res.data;
-  } catch (err) {
-    console.error('Login Failed:', err.response?.data || err.message);
-    throw err;
-  }
+  const res = await axios.post(`${API_BASE}/auth/login`, userData);
+  localStorage.setItem('token', res.data.token);
+  localStorage.setItem('user', JSON.stringify(res.data.user));
+  return res.data;
 };
