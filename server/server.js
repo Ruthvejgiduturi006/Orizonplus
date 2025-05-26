5d3ebaac0926071b09310d295ab2ec9305740663
Ruthvejgiduturi006 <Ruthvejgiduturi006@gmail.com>
2025-05-26 13:14:32 +0530

    sum

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ CORS Configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://orizonplus.netlify.app'], // Allow both local and production frontends
+  origin: ['http://localhost:5173','http://localhost:3000', 'https://orizonplus.netlify.app'], // Allow both local and production frontends
   credentials: true,
 }));
 
